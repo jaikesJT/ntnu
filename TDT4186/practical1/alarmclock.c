ebadd78242263499ee83ceb90fb1eb60b0f260e5
jaikesJT <jakob.tossebro@gmail.com>
2022-02-04 03:01:07 +0100

    deleted test files

--- TDT4186/practical1/alarmclock.c
+++ TDT4186/practical1/alarmclock.c
@@ -2,12 +2,18 @@
 #include <time.h>
 #include <string.h>
 
-int main() {
-    //finding current time in nice format
+//function to find time with appropriate format
+const char* time_str() {
     time_t mytime = time(NULL);
     char * time_str = ctime(&mytime);
     time_str[strlen(time_str)-1] = '\0';
 
-    printf("Welcome to the alarm clock! It is currently %s\n", time_str);
+    return time_str;
+}
+
+//main function, to create interface in shell 
+int main() {
+    printf("Welcome to the alarm clock! It is currently %s\n", time_str());
+    
     return 0;
 }
