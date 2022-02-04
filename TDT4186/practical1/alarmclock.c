f08832d71a1516275a9effd1e3f7ed6620b6b3f7
jaikesJT <jakob.tossebro@gmail.com>
2022-02-04 03:42:12 +0100

    began practical 1

--- TDT4186/practical1/alarmclock.c
+++ TDT4186/practical1/alarmclock.c
@@ -2,8 +2,8 @@
 #include <time.h>
 #include <string.h>
 
-//function to find time with appropriate format
-const char* time_str() {
+//function to find current time with appropriate format
+const char* currentTime() {
     time_t mytime = time(NULL);
     char * time_str = ctime(&mytime);
     time_str[strlen(time_str)-1] = '\0';
@@ -13,7 +13,32 @@ const char* time_str() {
 
 //main function, to create interface in shell 
 int main() {
-    printf("Welcome to the alarm clock! It is currently %s\n", time_str());
+    printf("Welcome to the alarm clock! It is currently %s\n", currentTime());
     
+    char command; //input character (user command)
+
+    printf("Please enter 's' (schedule), 'l' (list), 'c' (cancel), 'x' (exit)\n");
+    scanf("%c", &command);
+
+    while (command != 'x') {
+        if (command == 's') {
+            printf("Schedule alarm at which date and time?\n ");
+
+        } else if (command == 'l') {
+            printf("You chose 'l'\n");
+
+        } else if (command == 'c') {
+            int alarmNr;
+            printf("Cancel which alarm? ");
+            scanf("%d", &alarmNr);
+            printf("Alarm nr that is to be cancelled is: %d\n", alarmNr);
+        }
+
+        printf("Please enter 's' (schedule), 'l' (list), 'c' (cancel), 'x' (exit)\n");
+        scanf(" %c", &command);
+    }
+
+    printf("Goodbye!\n");
+
     return 0;
 }
